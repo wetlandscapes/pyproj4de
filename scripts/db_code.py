9a7fa1aff4cf66168454591682b4ee6e41c94fd9
Jason Mercer <jasonjmercer@gmail.com>
2025-02-10 09:03:17 -0500

    pixi(env): ibis-duckdb

--- scripts/db_code.py
+++ scripts/db_code.py
@@ -1,6 +1,35 @@
-import sqlite3
 import ibis
+import ibis.selectors as s
+from ibis import _
+import polars as pl
 
+df = ibis.read_csv("data/raw/minimal/INSTRUCTOR.csv",
+    column_names = ['ID', 'FNAME', 'LNAME', 'CITY', 'CCODE'])
 
-ibis.options.interactive = True
+#Return only FNAME
+(df.to_polars()
+    .select(["FNAME"])
+)
 
+ibis.to_sql(
+    (df.select(["FNAME"]))
+)
+
+#Return rows
+(df.to_polars()
+    .select(pl.len())
+)
+
+ibis.to_sql(
+    (df.count())
+)
+
+#Append
+df_row = pl.DataFrame(
+    {'ID': [100],
+    'FNAME': ['John'],
+    'LNAME': ['Doe'],
+    'CITY': ['Paris'],
+    'CCODE': ['FR']})
+
+df.union_all(df_row)
